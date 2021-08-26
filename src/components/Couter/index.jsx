8111710172834b8dedc722ba2f92c6c778ffc87e
Tima Rejepguliev <TimaRejepgulyew99>
2021-08-26 15:26:26 +0500

    add counter component use Hook functional component

--- src/components/Couter/index.jsx
+++ src/components/Couter/index.jsx
@@ -1,2 +1,17 @@
 import React, { useState } from 'react'
-function Counter () {}
+function Counter () {
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => setCounter(counter + 1)
+  const decrement = () => setCounter(counter - 1)
+
+  return (
+    <section>
+      <h2>{counter}</h2>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </section>
+  )
+}
+
+export default Counter

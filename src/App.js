8c0322af0e587603a95405a72e4988c150ecafaa
Tima Rejepguliev <TimaRejepgulyew99>
2021-08-26 18:20:40 +0500

    add global style and add post component in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import './App.css'
-import Counter from './components/Counter/index'
-import ClassCounter from './components/Counter-class/index'
+import React, { useState } from 'react'
 
+// import Counter from './components/Counter/index'
+// import ClassCounter from './components/Counter-class/index'
+import PostList from './components/Post/index'
 function App () {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'JavaScript',
+      description: 'Javascript язык программирование'
+    },
+    {
+      id: 2,
+      title: 'Python',
+      description: 'Python язык программирование'
+    },
+    {
+      id: 3,
+      title: 'Java',
+      description: 'Java язык программирование'
+    },
+    {
+      id: 4,
+      title: 'C#',
+      description: 'C# язык программирование'
+    }
+  ])
+  const addPost = post => {
+    setPosts([...posts, { ...post, id: posts.length }])
+  }
+
   return (
     <div className='App'>
-      <Counter />
-      <ClassCounter />
+      <PostList
+        addPost={addPost}
+        posts={posts}
+        title={'Post about programming'}
+      ></PostList>
     </div>
   )
 }
